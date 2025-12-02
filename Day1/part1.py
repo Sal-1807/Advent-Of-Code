6c01e9b0143dedcd02dae4318c87ee104bc7c064
Sal-1807 <salonipalo735@gmail.com>
2025-12-02 10:31:04 +0530

    first commit

---
+++ Day1/part1.py
@@ -0,0 +1,21 @@
+pos = 50
+count_zero = 0
+
+with open("data.txt") as f:
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+
+        direction = line[0]          # 'L' or 'R'
+        distance = int(line[1:])     # the number
+
+        if direction == 'L':
+            pos = (pos - distance) % 100
+        else:   # 'R'
+            pos = (pos + distance) % 100
+
+        if pos == 0:
+            count_zero += 1
+
+print("Password =", count_zero)
